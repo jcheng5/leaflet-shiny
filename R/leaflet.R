ddb15ce97baa9aed56d383be1e3a1d1f28e40263
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2014-11-11 12:53:12 +1000

    fix11

--- R/leaflet.R
+++ R/leaflet.R
@@ -103,8 +103,6 @@ leafletMap <- function(
     singleton(
       tags$head(
         tags$link(rel="stylesheet", type="text/css", href="leaflet/leaflet.css"),
-        tags$script(src="leaflet/leaflet-plugins/layer/tile/Google.js"),
-        tags$script(src="leaflet/leaflet-plugins/control/Layers.Load.js"),
         tags$script(src="leaflet/leaflet.js"),
         tags$script(src="leaflet/binding.js")
       )
